b291d043efaab6a2e054a509752e56505876689c
zhu xiaolong <zhuxiaolong05@126.com>
2016-12-25 17:25:45 +0800

    Support autowire based on xml config

---
+++ LessonFour/src/main/java/com/xiaomingyk/lessonFour/Class.java
@@ -0,0 +1,78 @@
+package com.xiaomingyk.lessonFour;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Created by My on 12/25/2016.
+ */
+public class Class {
+
+    private List<Person> students;
+    private Set<Person> teachers;
+    private Map<Lecture,Person> lectures;
+    private Properties properties;
+
+    public void setStudents(List<Person> personList){
+
+        System.out.println("Setting students.");
+
+        students = personList;
+    }
+
+
+    public List<Person> getStudents(){
+
+        System.out.println("Getting students."+ students.toString());
+
+        return students;
+    }
+
+    public void setTeachers(Set<Person> personList){
+
+        System.out.println("Setting teachers.");
+
+        teachers = personList;
+    }
+
+
+    public Set<Person> getTeachers(){
+
+        System.out.println("Getting teachers."+teachers.toString());
+
+        return teachers;
+    }
+
+    public void setLectures(Map<Lecture,Person> lectures){
+
+        System.out.println("Setting lectures.");
+
+        this.lectures = lectures;
+    }
+
+
+    public Map<Lecture,Person> getLectures(){
+
+        System.out.println("Getting lectures." + lectures.toString());
+
+        return lectures;
+    }
+
+    public void setProperties(Properties properties){
+
+        System.out.println("Setting properties.");
+
+        this.properties = properties;
+    }
+
+
+    public Properties getProperties(){
+
+        System.out.println("Getting properties." + properties.toString());
+
+        return properties;
+    }
+
+}
---
+++ LessonFour/src/main/java/com/xiaomingyk/lessonFour/Person.java
@@ -0,0 +1,35 @@
+package com.xiaomingyk.lessonFour;
+
+/**
+ * Created by My on 12/25/2016.
+ */
+public class Person {
+
+    private String name;
+    private String gender;
+
+    public void setGender(String gender) {
+        System.out.println("Setting person gender:" + gender);
+        this.gender = gender;
+    }
+
+    public String getGender() {
+
+        System.out.println("Getting person gender:" + gender);
+        return gender;
+    }
+
+    public void setName(String name) {
+
+        System.out.println("Setting person name:" + name);
+
+        this.name = name;
+    }
+
+    public String getName() {
+
+        System.out.println("Getting person name:" + name);
+
+        return name;
+    }
+}
